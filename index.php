12f5fad25db8f5d88510c733cd194a04c1253ee4
Will <willgrund178@gmail.com>
2023-09-19 14:05:27 -0400

    First commit

---
+++ index.php
@@ -0,0 +1,79 @@
+<?php
+$team = [
+    1 => [
+        'name' => 'Will Grund',
+        'role' => 'Software Engineer',
+        'image' => 'assets\images\profile.jpg',
+    ],
+    2 => [
+        'name' => 'Master Chief',
+        'role' => 'Spartan',
+        'image' => 'assets\images\halo_3_DMBwcnr.webp',
+    ],
+	3 => [
+        'name' => 'Beeg Yoshi',
+        'role' => 'Yoshi',
+        'image' => 'assets\images\Beeg_Yoshi.webp',
+    ],
+];
+
+?>
+<!DOCTYPE html>
+<html lang="en"> 
+<head>
+    <title>Our amazing team</title>
+    
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Your name's resume">
+    <meta name="author" content="Your name">    
+    <link rel="shortcut icon" href="favicon.ico"> 
+    
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900" rel="stylesheet">
+    
+    <!-- FontAwesome JS-->
+	<script defer src="assets/fontawesome/js/all.min.js"></script>
+       
+    <!-- Theme CSS -->  
+    <link id="theme-style" rel="stylesheet" href="assets/css/pillar-1.css">
+
+
+</head> 
+
+<body>
+    <article class="resume-wrapper text-center position-relative">
+	    <div class="resume-wrapper-inner mx-auto text-start bg-white shadow-lg">
+			<h1 class="py-4 text-center">OUR AMAZING TEAM</h1>
+		    <?php
+            foreach ($team as $id => $member) {
+                include 'template.php';
+                $template = str_replace('Member name', $team[$id]['name'], $template);
+                $template = str_replace('Member role', $team[$id]['role'], $template);
+                $template = str_replace('assets/images/profile.jpg', $team[$id]['image'], $template);
+
+                // Generate the detail page URL
+                $detailPageURL = "detail.php?id=$id";
+                $template = str_replace("LINK_TO_MEMBER_PAGE", $detailPageURL, $template);
+
+                echo $template;
+}
+?>
+
+		    
+	    </div>
+    </article> 
+
+    
+    <footer class="footer text-center pt-2 pb-5">
+	    <!--/* This template is free as long as you keep the footer attribution link. If you'd like to use the template without the attribution link, you can buy the commercial license via our website: themes.3rdwavemedia.com Thank you for your support. :) */-->
+        <small class="copyright">Designed with <span class="sr-only">love</span><i class="fas fa-heart"></i> by Your names</small>
+    </footer>
+
+    
+
+</body>
+</html> 
+
---
+++ template.php
@@ -0,0 +1,21 @@
+<?php $template = <<<HTML
+<header class="resume-header mt-4 pt-4 pt-md-0">
+    <div class="row">
+        <div class="col-block col-md-auto resume-picture-holder text-center text-md-start">
+            <img class="picture" src="" alt="">
+        </div><!--//col-->
+        <div class="col">
+            <div class="row p-4 justify-content-center justify-content-md-between">
+                <div class="primary-info col-auto">
+                    <h1 class="name mt-0 mb-1 text-white text-uppercase text-uppercase">Member name</h1>
+                    <div class="title mb-3">Member role</div>
+                    <a href="<?php LINK_TO_MEMBER_PAGE ?>" class="btn btn-secondary">See full profile</a>
+                </div><!--//primary-info-->
+                <div class="secondary-info col-auto mt-2">
+                </div><!--//secondary-info-->
+            </div><!--//row-->
+        </div><!--//col-->
+    </div><!--//row-->
+</header>
+HTML;
+?>
